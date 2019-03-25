31b4645581485ce540de408a0eb88687c1094e89
xiwang wang <simon9211@126.com>
2019-03-25 17:50:33 +0800

    file of add folder

---
+++ QRReader/scan/QRAddFolderController.swift
@@ -0,0 +1,30 @@
+//
+//  QRAddFolderController.swift
+//  QRReader
+//
+//  Created by xiwang wang on 2019/3/25.
+//  Copyright © 2019 com.simon. All rights reserved.
+//
+
+import UIKit
+
+class QRAddFolderController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
